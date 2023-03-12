3070af64b72712e000ce258a88c443f70b58c18d
Srijan Verma <verma.srijan19@gmail.com>
2023-03-12 11:21:01 +0530

    out

---
+++ out/_next/static/Y082Cgx8gyGGo9AsXr1Jm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/361-aa13a8b83dc288e5.js","static/chunks/pages/index-7ef040785095de5d.js"],"/_error":["static/chunks/pages/_error-0260c064403af775.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
